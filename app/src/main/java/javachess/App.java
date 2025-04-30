0977dd0957adb3bd68783d1195a34aa8224e100e
Clément <clement.reniers@etu.univ-lyon1.fr>
2025-04-30 15:57:07 +0200

    beginning of a global app

--- app/src/main/java/javachess/App.java
+++ app/src/main/java/javachess/App.java
@@ -1,7 +1,27 @@
 package javachess;
 
-public class App {
+import javax.swing.*;
+import java.awt.*;
+
+public class App extends JFrame {
     public static void main(String[] args) {
-        Game game = new Game();
+        new App();
+    }
+
+    public App() {
+        setTitle("Java Chess");
+        setIconImage(Toolkit.getDefaultToolkit().getImage(Window.class.getResource("/white-king.png")));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+
+        JLabel principalLabel = new JLabel(new ImageIcon(Toolkit.getDefaultToolkit().getImage(Window.class.getResource("/background.jpeg"))));
+        principalLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        principalLabel.setPreferredSize(new Dimension(800, 600));
+
+        add(principalLabel);
+
+
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
 }
